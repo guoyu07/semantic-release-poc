d0f9cc9e3bdad906cc733bdb996da38f35dcb5d5
Nishchal Gautam <gautam.nishchal@gmail.com>
2017-05-03 20:25:17 +0700

    testing travis deploy

--- .docker/release/semantic-release.sh
+++ .docker/release/semantic-release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if test $DO_PUBLISH; then
-    echo "{name: 'Slayer', repository: {type: 'git', 'url': 'https://github.com/cyberhck/semantic-release-poc.git'}}" > package.json
+    echo '{"name": "Slayer", "repository": {"type": "git", "url": "https://github.com/cyberhck/semantic-release-poc.git"}}' > package.json
     npm i -g npm
     npm install semantic-release last-release-git
     ./node_modules/.bin/semantic-release pre --get-last-release="last-release-git" && ./node_modules/.bin/semantic-release post --get-last-release="last-release-git"
