c32daef056c4e6565785a4dee18bbe4446c59fab
Nishchal Gautam <gautam.nishchal@gmail.com>
2017-05-03 20:47:00 +0700

    testing travis deploy

--- .docker/release/semantic-release.sh
+++ .docker/release/semantic-release.sh
@@ -2,4 +2,6 @@
 echo '{"name": "Slayer", "repository": {"type": "git", "url": "https://github.com/cyberhck/semantic-release-poc.git"}}' > package.json
 npm i -g npm
 npm install semantic-release last-release-git
-./node_modules/.bin/semantic-release pre --get-last-release="last-release-git" && ./node_modules/.bin/semantic-release post --get-last-release="last-release-git" && curl -X POST https://hooks.slack.com/services/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 -d '{"text": "All tests passed"}'
+./node_modules/.bin/semantic-release pre --get-last-release="last-release-git" \
+    && ./node_modules/.bin/semantic-release post --get-last-release="last-release-git" \
+    && curl -X POST https://hooks.slack.com/services/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 -d '{"text": "All tests passed"}'
