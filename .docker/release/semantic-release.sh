4d6bd3e24ae740ea6488ac8d22e467064e9c2237
Nishchal Gautam <gautam.nishchal@gmail.com>
2017-05-03 19:43:41 +0700

    testing travis deploy

--- .docker/release/semantic-release.sh
+++ .docker/release/semantic-release.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 if test $DO_PUBLISH; then
+    npm init -y;
+    npm install semantic-release last-release-git;
+    ./node_modules/.bin/semantic-release pre --get-last-release="last-release-git" && ./node_modules/.bin/semantic-release post --get-last-release="last-release-git"
     curl -X POST https://hooks.slack.com/services/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 -d '{"text": "All tests passed"}'
 fi
