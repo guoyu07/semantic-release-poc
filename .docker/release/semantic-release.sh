414c962fb296c5030b480fd583c04a15c7db05f2
Nishchal Gautam <gautam.nishchal@gmail.com>
2017-05-03 20:08:45 +0700

    testing travis deploy

--- .docker/release/semantic-release.sh
+++ .docker/release/semantic-release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if test $DO_PUBLISH; then
+    echo "{}" > package.json
     npm i -g npm
     npm install semantic-release last-release-git
     ./node_modules/.bin/semantic-release pre --get-last-release="last-release-git" && ./node_modules/.bin/semantic-release post --get-last-release="last-release-git"
