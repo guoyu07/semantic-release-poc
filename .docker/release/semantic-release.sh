8623a967fd78e5dd470d3e4a3c605a964a029bd3
Nishchal Gautam <gautam.nishchal@gmail.com>
2017-05-03 19:26:47 +0700

    testing travis deploy

--- .docker/release/semantic-release.sh
+++ .docker/release/semantic-release.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-curl -X POST https://hooks.slack.com/services/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 -d '{"text": "All tests passed"}'
-echo $DO_PUBLISH
+if test $DO_PUBLISH; then
+    curl -X POST https://hooks.slack.com/services/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 -d '{"text": "All tests passed"}'
+fi
